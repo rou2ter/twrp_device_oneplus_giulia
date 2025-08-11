d4d5c7818352ed8bb78283b0a0879d3704ccbef9
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-08-11 20:01:06 +0800

    Update variant-script.sh
    Add OnePlus 13 R CPH2647 23869 (NA)

--- recovery/root/system/bin/variant-script.sh
+++ recovery/root/system/bin/variant-script.sh
@@ -37,6 +37,23 @@ echo $variant
                 resetprop ro.product.odm.model "CPH2645"
                 resetprop ro.boot.hardware.revision "GL"
                 ;;
+            "23869")
+            # oneplus 13 R (giulia)
+                echo "Oneplus 13 R" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "OP5D3BL1"
+                resetprop ro.product.system.device "CPH2647"
+                resetprop ro.product.vendor.device "OP5D3BL1"
+                resetprop ro.product.odm.device "OP5D3BL1"
+                resetprop ro.product.product.device "OP5D3BL1"
+                resetprop ro.product.system_ext.device "OP5D3BL1"
+                resetprop ro.product.product.model "CPH2647"
+                resetprop ro.product.model "CPH2647"
+                resetprop ro.product.system.model "CPH2647"
+                resetprop ro.product.system_ext.model "CPH2647"
+                resetprop ro.product.vendor.model "CPH2647"
+                resetprop ro.product.odm.model "CPH2647"
+                resetprop ro.boot.hardware.revision "NA"
+                ;;
             "23867")
             # oneplus 13 R CN (giulia)
                 echo "Oneplus 13 R" > /config/usb_gadget/g1/strings/0x409/product
